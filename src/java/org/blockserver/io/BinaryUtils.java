06e5892ab5471ee2fcf0ed2b0457e34e79b5b065
PEMapModder <pemapmodder1970@gmail.com>
2014-10-21 20:30:07 +0800

    Un-revert!

--- src/java/org/blockserver/io/BinaryUtils.java
+++ src/java/org/blockserver/io/BinaryUtils.java
@@ -15,8 +15,8 @@ public abstract class BinaryUtils{
 	public static long read(byte[] buffer, int start, int length){
 		long x = 0;
 		for(int i = 0; i < length; i++){
-			x |= buffer[start + i];
 			x <<= 8;
+			x |= buffer[start + i];
 		}
 		return x;
 	}
